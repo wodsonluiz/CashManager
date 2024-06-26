b9816cbf8992b770dacc298ef0a9cbf10fecc23f
wodson <wodsonluiz@live.com>
2024-06-26 16:04:54 -0300

    fix: Ajusting connection for rabbitmq

--- src/CashManager.Infrastructure/RabbitMq/ProducerMessageHandler.cs
+++ src/CashManager.Infrastructure/RabbitMq/ProducerMessageHandler.cs
@@ -7,11 +7,17 @@ namespace CashManager.Infrastructure.RabbitMq
     public class ProducerMessageHandler: IProducerMessageHandler
     {
         private readonly IBus _bus;
+        private readonly RabbitMqOptions _options;
         private readonly Exchange _exchange;
 
         public ProducerMessageHandler(IBus bus, RabbitMqOptions options)
         {   
-            _exchange = new Exchange(options.ExchangeName, options.ExchangeType, true, false);
+            _exchange = bus.Advanced.ExchangeDeclare(options.ExchangeName, ExchangeType.Direct);
+            var queue = bus.Advanced.QueueDeclare(options.QueueName);
+
+            bus.Advanced.Bind(_exchange, queue, options.RoutingKey);
+
+            _options = options;
             _bus = bus;
         }
 
@@ -19,7 +25,9 @@ namespace CashManager.Infrastructure.RabbitMq
         {
             var message = new Message<T>(entity);
 
-            return _bus.Advanced.PublishAsync(_exchange, "transactions.#", false, message);
+            return _bus.Advanced.PublishAsync(message: message, exchange: _exchange, routingKey: _options.RoutingKey, mandatory: false);
+
+
         }
     }
 }
