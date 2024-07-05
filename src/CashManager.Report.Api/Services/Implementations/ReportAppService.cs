8ac1c4f4a0df5c75b3d2fbf1ce2d600781d858e8
Wodson Correia <wodsonluiz@live.com>
2024-07-05 17:22:50 -0300

    fix: Remove exception (#6)

--- src/CashManager.Report.Api/Services/Implementations/ReportAppService.cs
+++ src/CashManager.Report.Api/Services/Implementations/ReportAppService.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
-using Amazon.SecurityToken.Internal;
 using CashManager.Domain.ReportAgg;
 using CashManager.Infrastructure.Repository;
 using CashManager.Report.Api.Models;
@@ -46,10 +45,11 @@ namespace CashManager.Report.Api.Services.Implementations
 
         private ReportContentResponse BuildReport(IEnumerable<ReportDaily> reportsCredit, IEnumerable<ReportDaily> reportsDebit)
         {
-            if (reportsCredit == null && reportsDebit == null)
-                throw new ArgumentException("Both reportsCredit and reportsDebit cannot be null");
-
             var reportResponse = new ReportContentResponse();
+
+            if (reportsCredit == null && reportsDebit == null)
+                return reportResponse;
+            
             var customerDetails = reportsCredit?.FirstOrDefault() ?? reportsDebit?.FirstOrDefault();
 
             long debited = reportsDebit?.Sum(r => r.Transaction.Amount) ?? 0;
@@ -58,20 +58,18 @@ namespace CashManager.Report.Api.Services.Implementations
             reportResponse.Name = customerDetails?.Name;
             reportResponse.Document = customerDetails?.Document;
             reportResponse.Balance = credited - debited;
-            reportResponse.Transactions = new List<TransactionContentResponse>();
+            reportResponse.Transactions = [];
 
             var allReports = reportsCredit?.Concat(reportsDebit) ?? Enumerable.Empty<ReportDaily>();
 
-            foreach (var report in allReports)
+            foreach (var transaction in allReports.Select(r => r.Transaction))
             {
-                var transaction = new TransactionContentResponse
+                reportResponse.Transactions.Add(new TransactionContentResponse
                 {
-                    Amount = report.Transaction.Amount,
-                    DateOperation = report.Transaction.DateOperation,
-                    OperationType = report.Transaction.OperationType
-                };
-
-                reportResponse.Transactions!.Add(transaction);
+                    Amount = transaction.Amount,
+                    DateOperation = transaction.DateOperation,
+                    OperationType = transaction.OperationType
+                });
             }
 
             return reportResponse;
@@ -80,7 +78,7 @@ namespace CashManager.Report.Api.Services.Implementations
         private Expression<Func<ReportDaily, bool>> GetFilter(string document, string operationType)
         {
             Expression<Func<ReportDaily, bool>> filter = report => report.Document == document 
-                && report.Transaction.OperationType.ToLower() == operationType;
+                && report.Transaction.OperationType.Equals(operationType, StringComparison.CurrentCultureIgnoreCase);
 
             return filter;
         }
